58c21d07ead7bffff8290949d86550e8f9682771
Johan Jeppsson <johjep@gmail.com>
2017-08-04 10:45:16 +0200

    Rework pattern generation and add center alignments

--- termplotlib/raster.py
+++ termplotlib/raster.py
@@ -21,10 +21,16 @@ class Canvas(object):
 
     # Possible alignment values and their paddings used when stretching
     # the canvas.
-    _alignments = {'bottomleft' : np.array([[0, 1],[0, 1]]),
-                   'topleft'    : np.array([[1, 0],[0, 1]]),
-                   'bottomright': np.array([[0, 1],[1, 0]]),
-                   'topright'   : np.array([[1, 0],[1, 0]])}
+    _alignments = {'bottomleft'   : np.array([[0, 1],    [0, 1]]),
+                   'topleft'      : np.array([[1, 0],    [0, 1]]),
+                   'bottomright'  : np.array([[0, 1],    [1, 0]]),
+                   'topright'     : np.array([[1, 0],    [1, 0]]),
+                   'center'       : np.array([[0.5, 0.5],[0.5, 0.5]]),
+                   'centerleft'   : np.array([[0.5, 0.5],[0, 1]]),
+                   'centerright'  : np.array([[0.5, 0.5],[1, 0]]),
+                   'bottomcenter' : np.array([[0, 1],    [0.5, 0.5]]),
+                   'topcenter'    : np.array([[1, 0],    [0.5, 0.5]]),
+    }
 
     def __init__(self, width, height, pattern=None, background=None):
         if pattern is not None:
@@ -42,40 +48,64 @@ class Canvas(object):
         else:
             self.background = ''
 
-    def _set_size(self, width, height):
-        self.width = width
-        self.height = height
+    def _get_cell_dim(self, width, height):
+        c_width  = np.ceil(width/2.0).astype(int)
+        c_height = np.ceil(height/4.0).astype(int)
+        return c_width, c_height
 
-        self._c_width  = np.ceil(width/2.0).astype(int)
-        self._c_height = np.ceil(height/4.0).astype(int)
+    def _set_size(self, width, height):
+        self.width, self.height = width, height
+        self._c_width, self._c_height = self._get_cell_dim(width, height)
 
     def reset(self):
         self.pattern = np.zeros((self.height, self.width), dtype=bool)
         self._color_map = np.chararray((self.height, self.width), itemsize=15)
 
     def stretch(self, new_width, new_height, alignment='bottomleft'):
-        assert new_width >= self.width and new_height >= self.height
-        assert alignment in self._alignments.keys()
-        padding = self._alignments[alignment]
-        padding[0,:] *= new_height - self.height
-        padding[1,:] *= new_width - self.width
+        padding = self.get_padding(new_width, new_height, alignment)
         self.pattern = np.pad(self.pattern, padding, mode='constant')
         self._color_map = np.pad(self._color_map, padding, mode='constant')
+        self._set_size(new_width, new_height)
+
+    def get_padding(self, new_width, new_height, alignment='bottomleft'):
+        assert new_width >= self.width and new_height >= self.height
+        assert alignment in self._alignments.keys()
+
+        # Calculate padding based on the chosen alignment.
+        padding = self._alignments[alignment].copy()
+        padding[0,:] *= new_height - self.height
+        padding[1,:] *= new_width - self.width
+
+        # Make sure that we have integer padding, and that
+        # the total padding results in the correct width/height.
+        padding[0,0] = np.ceil(padding[0,0])
+        padding[0,1] = np.floor(padding[0,1])
+        padding[1,0] = np.ceil(padding[1,0])
+        padding[1,1] = np.floor(padding[1,1])
+
+        return padding.astype(int)
 
     def set(self, x, y, color=None):
-        # Find cell coordinates.
-        assert 0 <= x < self.width and 0 <= y < self.height
-        self.pattern[y, x] = True
-        if color:
-            self._color_map[y, x] = fg[color]
+        if hasattr(x, '__iter__'):
+            assert hasattr(y, '__iter__') and len(y) == len(x)
+        else:
+            x = [x]
+            y = [y]
+        for xi, yi in zip(x, y):
+            if xi >= self.width or yi >= self.height:
+                raise ValueError('Coordinate ({}, {})is outside canvas size ({}, {})'
+                                 .format(xi, yi, self.width, self.height))
+            self.pattern[yi, xi] = True
+            if color:
+                self._color_map[yi, xi] = fg[color]
 
-    def _get_cell_pattern(self, x, y):
-        pattern = self.pattern[y*4:(y+1)*4, x*2:(x+1)*2]
+    def _get_cell_pattern(self, x, y, full_pattern):
+        pattern = full_pattern[y*4:(y+1)*4, x*2:(x+1)*2]
         h, w = pattern.shape
         return np.pad(pattern, ((0, 4-h),(0, 2-w)), mode='constant')
 
-    def _get_cell_color(self, x, y):
-        colors = self._color_map[y*4:(y+1)*4, x*2:(x+1)*2].flatten()
+    def _get_cell_color(self, x, y, full_cmap):
+        colors = full_cmap[y*4:(y+1)*4, x*2:(x+1)*2].flatten()
         # Get the most used color for the cell
         counts = {}
         for c in colors:
@@ -85,46 +115,56 @@ class Canvas(object):
             return sorted(counts, key=counts.get, reverse=True)[0]
         return ''
 
-    def _to_unicode(self):
+    def get_rows(self, width=None, height=None, alignment='bottomleft'):
+        if width is None:
+            width = self.width
+        if height is None:
+            height = self.height
+        assert width >= self.width and height >= self.height
+        padding = self.get_padding(width, height, alignment)
+        pattern = np.pad(self.pattern, padding, mode='constant')
+        color_map = np.pad(self._color_map, padding, mode='constant')
+
+        c_width, c_height = self._get_cell_dim(width, height)
+
         rows = []
-        for y in np.arange(self._c_height):
+        for y in np.arange(c_height):
             row = self.background.encode('utf-8')
-            for x in np.arange(self._c_width):
+            for x in np.arange(c_width):
                 code = 0
-                for dot in (self._dots * self._get_cell_pattern(x, y)).flatten():
+                for dot in (self._dots * self._get_cell_pattern(x, y, pattern)).flatten():
                     code |= dot
                 if code == 0:
                     row += UNICODE_SPACE
                 else:
-                    color = self._get_cell_color(x, y)
+                    color = self._get_cell_color(x, y, color_map)
                     row += color.encode('utf-8')
                     row += unichr(code).encode('utf-8')
                     row += fg.RESET.encode('utf-8')
             row += bg.RESET.encode('utf-8')
             rows.append(row)
+        return rows
+
+    def _to_unicode(self):
+        rows = self.get_rows()
         return UNICODE_NEWLINE.join(reversed(rows))
 
     def __str__(self):
         return self._to_unicode()
 
-# class Layout(object):
-#     """Class containing and aligning multiple canvas objects."""
-
-#     def add
-
 if __name__ == '__main__':
 
-    canvas = Canvas(110, 110, background='grey')
+    canvas = Canvas(100, 100, background='grey')
     x = np.linspace(0, 2*np.pi, 100)
-    y = 48 * np.sin(x) + 50
-    y2 = 48 * np.cos(x) + 50
-    y3 =  (x - np.pi)**3
+    y = (48 * np.sin(x) + 50).astype(int)
+    y2 = (48 * np.cos(x) + 50).astype(int)
+    y3 =  ((x - np.pi)**3).astype(int)
     y3 -= min(y3)
-    for i, (y_, y2_, y3_) in enumerate(zip(y, y2, y3)):
-        canvas.set(i, int(y_), color='red')
-        canvas.set(i, int(y2_), color='green')
-        canvas.set(i, int(y3_), color='orange')
-
+    canvas.set(range(len(y)), y, color='red')
+    canvas.set(range(len(y2)), y2, color='green')
+    canvas.set(range(len(y3)), y3, color='orange')
+    canvas.set(75, 75, color='purple')
+    canvas.stretch(140, 140, 'center')
     print canvas
 
     from letters import Text
